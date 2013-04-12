ad40b07897d3903680ed7f7a52173add13537666
Osni Oliveira <osni.oliveira@caelum.com.br>
2013-04-12 19:59:48 -0300

    Criação da view para mostrar lista de Blog Posts; request e parsing de JSON para objetos Foundation (NSDictionary, NSArray)

---
+++ Carangos/BlogPostsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BlogPostsViewController.h
+//  Carangos
+//
+//  Created by Osni Oliveira on 12/04/13.
+//  Copyright (c) 2013 Starfuckers Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlogPostsViewController : UIViewController
+
+@end
---
+++ Carangos/BlogPostsViewController.m
@@ -0,0 +1,38 @@
+//
+//  BlogPostsViewController.m
+//  Carangos
+//
+//  Created by Osni Oliveira on 12/04/13.
+//  Copyright (c) 2013 Starfuckers Inc. All rights reserved.
+//
+
+#import "BlogPostsViewController.h"
+
+@interface BlogPostsViewController ()
+
+@end
+
+@implementation BlogPostsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
